b957df3c9914cd7342ce7f8496d2165b04e3e298
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2023-11-28 22:16:13 +0000

    Tests modified to include comments

--- Calculator/src/test/java/application/TestRevPolishCalc.java
+++ Calculator/src/test/java/application/TestRevPolishCalc.java
@@ -12,12 +12,13 @@ class TestRevPolishCalc {
   @BeforeEach
   void testConstructor() {
     testCalc = new RevPolishCalc();
+    assertNotNull(testCalc, "Constructor test");
   }
   
   //Test2
   @Test
   void testEvaluate() {
-    assertEquals(5, testCalc.evaluate("0 5"));
+    assertEquals(5, testCalc.evaluate("0 5 +"), "Test 0 + 5 = 5");
   }
 
 }
