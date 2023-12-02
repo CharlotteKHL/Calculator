9a83c1079bd9c2f02e1a8cbe8993b469a875b099
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2023-12-02 21:28:37 +0000

    New junit class created to test CalcModel class

---
+++ Calculator/src/test/java/application/TestCalcModel.java
@@ -0,0 +1,14 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestCalcModel {
+
+  //Test1
+  @Test
+  void testConstructor() {
+    CalcModel testCalc = new CalcModel();
+  }
+
+}
