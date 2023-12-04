2db985ba9e575845814c4075b004fc53a0fb3af2
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2023-12-04 23:43:56 +0000

    Tests refactored to initialise variables for later tests to use

--- Calculator/src/test/java/application/TestCalcController.java
+++ Calculator/src/test/java/application/TestCalcController.java
@@ -1,16 +1,25 @@
 package application;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestCalcController {
 
+  private CalcView testView;
+  private CalcModel testModel;
+  private CalcController testCalc;
+  
+  //Initialise Variables
+  @BeforeEach
+  void initialise() {
+    testView = new CalcView();
+    testModel = new CalcModel();
+    testCalc = new CalcController(testModel, testView);
+  }
   //Test1
   @Test
   void testConstructor() {
-    CalcView testView = new CalcView();
-    CalcModel testModel = new CalcModel();
-    CalcController testCalc = new CalcController(testModel, testView);
     assertNotNull(testCalc);
   }
 
