3636ba439f10e711cb84abd189dd896383d496a2
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2023-12-07 18:19:48 +0000

    After Documentation testing, javadoc updated in CalcController

--- Calculator/src/main/java/application/CalcController.java
+++ Calculator/src/main/java/application/CalcController.java
@@ -3,12 +3,18 @@ package application;
 /**
  * The controller that sits between the calculator model that does actual evaluation and the view
  * that is the part the user interfaces with.
+ * 
+ * @author Charlotte Lam
  */
 public class CalcController {
   private CalcModel myModel;
   private ViewInterface myView;
   private static boolean isInfix = false;
 
+  /**
+   * Runs when an expression needs to be evaluated from the View.
+   * 
+   */
   private void handleCalculation() {
     myView.setErrorMessage("");
     
@@ -28,6 +34,11 @@ public class CalcController {
 
   }
 
+  /**
+   * Runs when the View changes calculation type.
+   * 
+   * @param typeOfCalc is the type of calculator the View is now using
+   */
   private void handleTypeChange(OpType typeOfCalc) {
     if (typeOfCalc == OpType.REV_POLISH) {
       isInfix = false;
@@ -36,6 +47,12 @@ public class CalcController {
     }
   }
 
+  /**
+   * Constructor for CalcController.
+   * 
+   * @param model is the model called to evaluate expression in the MVC pattern
+   * @param view is the view being observed and displaying results in the MVC pattern
+   */
   CalcController(CalcModel model, ViewInterface view) {
     myModel = model;
     myView = view;
@@ -45,14 +62,29 @@ public class CalcController {
 
   }
   
+  /**
+   * Getter for the model the controller is using.
+   * 
+   * @return the model the controller is using
+   */
   public CalcModel getModel() {
     return myModel;
   }
   
+  /**
+   * Getter for the view the controller is using.
+   * 
+   * @return the view the controller is using
+   */
   public ViewInterface getView() {
     return myView;
   }
 
+  /**
+   * Getter for the type of calculation the controller is using.
+   * 
+   * @return true if in standard form, false if in reverse polish form
+   */
   public static boolean getOpType() {
     return isInfix;
   }
