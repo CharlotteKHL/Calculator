bdbfe0e29319fb18a40fe0cbec22dd52a5509685
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2023-12-09 23:43:28 +0000

    Tests modified to initialise an instance of StandardCalc for later tests to use

--- Calculator/src/test/java/application/TestStandardCalc.java
+++ Calculator/src/test/java/application/TestStandardCalc.java
@@ -1,14 +1,22 @@
 package application;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestStandardCalc {
+  
+  private StandardCalc testCalc;
 
+  @BeforeEach
+  void initialise() {
+    testCalc = new StandardCalc();
+  }
+  
   //Test1
   @Test
   void testConstructor() {
-    StandardCalc testCalc = new StandardCalc();
+    testCalc = new StandardCalc();
     assertNotNull(testCalc);
   }
 
