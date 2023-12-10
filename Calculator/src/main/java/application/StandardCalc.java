cb2a9be74e9c3c15c6f7f84e92f512adfec6ed65
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2023-12-10 10:12:31 +0000

    Evaluate can now calculate addition in standard form using a simplified Dijkstras shunting-yard algorithm. The algorithm was chosen due to the efficiency of calculating in postfix

--- Calculator/src/main/java/application/StandardCalc.java
+++ Calculator/src/main/java/application/StandardCalc.java
@@ -2,11 +2,62 @@ package application;
 
 /**
  * Calculator responsible with evaluating expressions in Standard form.
+ * 
+ * @author The below algorithm to evaluate is a simplified version of
+ *      Dijkstra's shunting-yard algorithm used to convert infix notation to postfix.
+ *     
+ * @author Descriptions used to guide my programming can be found at these two links:
+ *     https://www.chris-j.co.uk/parsing.php
+ *     https://brilliant.org/wiki/shunting-yard-algorithm/
+ *         
  */
 public class StandardCalc {
+  
+  private OpStack values = new OpStack();
+  private RevPolishCalc rpCalc = new RevPolishCalc();
+  private String output;
 
-  public float evaluate(String expression) {
-    return 4f;
+  /**
+   * Evaluates an expression in Standard form and returns answer.
+   * 
+   * @param expression is the string to be evaluated
+   * @return the result of the evaluation of the expression in standard form
+   * @throws BadTypeException if getter for Entry does not match type of object
+   * @throws EmptyStackException if pop or top called on an empty stack
+   * @throws InvalidExpressionException if expression is invalid in standard form
+   */
+  public float evaluate(String expression) 
+      throws EmptyStackException, BadTypeException, InvalidExpressionException {
+    
+    String[] list = expression.split(" ");
+    output = "";
+    
+    for (int index = 0; index < list.length; index++) {
+      
+      switch(list[index]) {
+        
+        case "+":
+          values.push(Symbol.PLUS);
+          break;
+          
+        default:
+          if (output == "") {
+            output = list[index];
+          } else {
+            output = output + " " + list[index];
+          }
+          
+      }
+    }
+    
+    for (int index = 0; index <= values.size(); index++) {
+      values.pop();
+      output = output + " +";
+    }
+
+    float answer = rpCalc.evaluate(output);
+    
+    return answer;
   }
 
 }
--- Calculator/src/test/java/application/TestStandardCalc.java
+++ Calculator/src/test/java/application/TestStandardCalc.java
@@ -22,8 +22,17 @@ class TestStandardCalc {
   
   //Test2
   @Test
-  void testEvaluateSimple() {
+  void testEvaluateSimple() throws EmptyStackException, BadTypeException, InvalidExpressionException  {
     assertEquals(4f, testCalc.evaluate("2 + 2"), "Test that 2 + 2 = 4");
+  }
+  
+  //Test3
+  @Test
+  void testEvaluateAddMutliple() throws EmptyStackException, BadTypeException, InvalidExpressionException  {
+    assertEquals(2f, testCalc.evaluate("1 + 1"), "Test that 1 + 1 = 2");
+    assertEquals(25f, testCalc.evaluate("10 + 15"), "Test that 10 + 15 = 25");
+    assertEquals(30f, testCalc.evaluate("10 + 10 + 10"), "Test that 10 + 10 + 10 = 30");
+    
   }
 
 }
